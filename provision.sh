c9ac474e9f21e040f98dc7225eb64309fb49393c
Demi <demetrajo@gmail.com>
2021-05-29 17:03:41 -0400

    changes to vm

--- provision.sh
+++ provision.sh
@@ -1,23 +1,11 @@
 apt-get update && apt-get -y upgrade
 
 # Install psql
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
-apt-get update
+
 apt-get install -y \
-  postgresql-13 \
-  postgresql-client-13 \
   python3 \
   python3-pip \
   dos2unix
-
-# Make accessible from host
-echo "host    all         all         0.0.0.0/0             trust" >> /etc/postgresql/13/main/pg_hba.conf
-echo "listen_addresses = '*'" >> /etc/postgresql/13/main/postgresql.conf
-service postgresql restart
-
-# Create Role and login
-su postgres -c "psql -w -c \"CREATE ROLE vagrant SUPERUSER LOGIN PASSWORD 'vagrant'\" && createdb vagrant"
 
 # rename python3 stuff to just python
 ln -s /usr/bin/python3 /usr/bin/python
